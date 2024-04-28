7dc5eb5e1f9ecad818ba2b077a1b1b8432b45006
Soulztorm <FRL@gmx.de>
2024-04-29 01:19:31 +0200

    Bump to version 1.6.2

--- ChooChoo-TraderModding/Properties/AssemblyInfo.cs
+++ ChooChoo-TraderModding/Properties/AssemblyInfo.cs
@@ -4,10 +4,10 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.Versioning;
 
-[assembly: AssemblyVersion("1.6.1")]
+[assembly: AssemblyVersion("1.6.2")]
 [assembly: AssemblyCompany("Choo²")]
 [assembly: AssemblyConfiguration("Release")]
-[assembly: AssemblyFileVersion("1.6.1")]
-[assembly: AssemblyInformationalVersion("1.6.1")]
+[assembly: AssemblyFileVersion("1.6.2")]
+[assembly: AssemblyInformationalVersion("1.6.2")]
 [assembly: AssemblyProduct("Choo²-TraderModding")]
 [assembly: AssemblyTitle("Choo²-TraderModding")]
